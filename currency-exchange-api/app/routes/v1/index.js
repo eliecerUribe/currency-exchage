5673c94c317838d9e1c9c0c0faf728e4973f9ace
Eliecer Uribe <eliecer.uribe@tektonlabs.com>
2018-12-18 02:48:58 -0500

    remove validation with token for currency endpoint

--- currency-exchange-api/app/routes/v1/index.js
+++ currency-exchange-api/app/routes/v1/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const passport = require('passport');
+// const passport = require('passport');
 
 const router = express.Router();
 
@@ -8,6 +8,7 @@ const currencyRoutes = require('./currency');
 const { validPayload } = require('../../middlewares');
 
 router.use('/auth', validPayload, authRoutes);
-router.use('/currency', passport.authenticate('jwt', { session: false }), currencyRoutes);
+router.use('/currency', currencyRoutes);
+// router.use('/currency', passport.authenticate('jwt', { session: false }), currencyRoutes);
 
 module.exports = router;
